21ad7edcb6016a906ba90b475fb0659af6374aed
Alessio <alexeimv91@gmail.com>
2025-04-24 23:55:43 -0400

    feat(auth & shared): añadir decorador y guardia de roles para control de acceso

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,13 +15,14 @@ export class AuthService {
     if (user && (await bcrypt.compare(pass, user.password))) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
+      console.log(result);
       return result;
     }
     throw new UnauthorizedException('Credenciales incorrectas');
   }
 
   async login(user: any) {
-    const payload = { username: user.email, sub: user.id, role: user.role };
+    const payload = { email: user.email, id: user.id, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
     };
---
+++ src/shared/shared.module.ts
@@ -0,0 +1,9 @@
+// src/common/common.module.ts
+import { Module } from '@nestjs/common';
+import { RolesGuard } from './guards/roles.guard';
+
+@Module({
+  providers: [RolesGuard],
+  exports: [RolesGuard], // ¡exporta para que otros módulos puedan usarlo!
+})
+export class CommonModule {}
